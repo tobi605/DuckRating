20e8f1326b78da9c937fcefb23360c2060e49985
diamentowygimbus <tobi605@gmail.com>
2018-04-17 22:34:44 +0200

    setup base ui

--- app/src/main/java/com/diamentowygimbus/duckrating/MainActivity.java
+++ app/src/main/java/com/diamentowygimbus/duckrating/MainActivity.java
@@ -1,13 +1,76 @@
 package com.diamentowygimbus.duckrating;
 
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private int container;
+    private android.support.v4.app.FragmentManager fragmentManager;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        container = R.id.main_container;
+        fragmentManager = getSupportFragmentManager();
+        toolbar = findViewById(R.id.toolbar);
+
+        initFragmentTransaction();
+        setupToolbar();
+        addNavigationListeners();
+
+
+    }
+
+    private void initFragmentTransaction() {
+        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(container, MainFragment.newInstance(getApplicationContext()));
+        transaction.commit();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+
+    private void addNavigationListeners() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                //TODO switch fragments
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/diamentowygimbus/duckrating/MainFragment.java
@@ -0,0 +1,55 @@
+package com.diamentowygimbus.duckrating;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link //MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends Fragment {
+    private static Context appContext;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance(Context context) {
+        MainFragment fragment = new MainFragment();
+        appContext = context;
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+}
